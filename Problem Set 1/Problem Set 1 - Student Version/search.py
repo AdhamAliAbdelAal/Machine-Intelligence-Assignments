c0771c3fcc015024ba6e1847e9e6dd36a8c9ec5d
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-11-03 18:20:11 +0200

    bfs 8/8

--- Problem Set 1/Problem Set 1 - Student Version/search.py
+++ Problem Set 1/Problem Set 1 - Student Version/search.py
@@ -17,24 +17,27 @@ import heapq
 
 def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     #TODO: ADD YOUR CODE HERE
-    frontier: Queue[list[S]] = Queue()
-    frontier.put([initial_state])
+    frontier: Queue[S] = Queue()
+    paths : Queue[list[A]] = Queue()
+    frontier.put(initial_state)
+    paths.put([])
     explored = set()
     while not frontier.empty():
-        curr_path = frontier.get()
-        state = curr_path[-1]
+        state = frontier.get()
+        path = paths.get()
         if state in explored:
             continue
         explored.add(state)
         actions = problem.get_actions(state)
         for action in actions:
             successor = problem.get_successor(state, action)
-            new_path = list(curr_path)
-            new_path.append(successor)
+            new_path = list(path)
+            new_path.append(action)
             if problem.is_goal(successor):
-                return new_path[1:]
+                return new_path
             if successor not in explored:
-                frontier.put(new_path)
+                frontier.put(successor)
+                paths.put(new_path)
     return None
 
 def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
