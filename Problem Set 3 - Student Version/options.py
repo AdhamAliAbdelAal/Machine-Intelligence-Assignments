501e3b6276f32de4ff7dff1b1dabd11d41d897ee
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-12-28 19:01:19 +0200

    4/6 problem2

--- Problem Set 3 - Student Version/options.py
+++ Problem Set 3 - Student Version/options.py
@@ -5,22 +5,22 @@ def question2_1():
     return {
         "noise": 0,
         "discount_factor": 0.1,
-        "living_reward": -0.4
+        "living_reward": 0
     }
 
 def question2_2():
     #TODO: Choose options that would lead to the desired results
     return {
         "noise": 0,
-        "discount_factor": 1,
-        "living_reward": 0
+        "discount_factor": 0.99,
+        "living_reward": 0.1
     }
 
 def question2_3():
     #TODO: Choose options that would lead to the desired results
     return {
         "noise": 0,
-        "discount_factor": 1,
+        "discount_factor": 0.9,
         "living_reward": 0
     }
 
@@ -37,7 +37,7 @@ def question2_5():
     return {
         "noise": 0,
         "discount_factor": 1,
-        "living_reward": 0
+        "living_reward": 10
     }
 
 def question2_6():
@@ -45,5 +45,5 @@ def question2_6():
     return {
         "noise": 0,
         "discount_factor": 1,
-        "living_reward": 0
+        "living_reward": -100
     }
